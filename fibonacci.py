d5ecc93a5c645a89d6b4962c9b316c33149c117c
Edith <mxedithbaltazar@gmail.com>
2023-06-05 11:55:45 -0700

    first commit

---
+++ fibonacci.py
@@ -0,0 +1,43 @@
+def fibonacci(n, memo=None):
+    if n == 0 or n == 1:
+        return n
+    
+    if memo is not None:
+        memo = memo
+    else:
+        memo = {}
+
+    if n in memo:
+        return memo[n]
+
+    # return (fibonacci(n-1) + fibonacci(n-2))
+    # return (fibonacci(n-1, memo) + fibonacci(n-2, memo))
+    result = (fibonacci(n-1, memo) + fibonacci(n-2, memo))
+
+    memo[n] = result
+    print('this is my memo', memo)
+    return result
+
+# same results with below code
+# use cache decorator
+from functools import cache
+
+rec_total_calls = 0
+rec_calls_per_n = {}
+
+def fibonacci(n, memo=None):
+    if n == 0 or n == 1:
+        return n
+    
+    if memo is not None:
+        memo = memo
+    else:
+        memo = {}
+
+    if n in memo:
+        return memo[n]
+
+    # return (fibonacci(n-1) + fibonacci(n-2))
+    # return (fibonacci(n-1, memo) + fibonacci(n-2, memo))
+    # result = (fibonacci(n-1, memo) + fibonacci(n-2, memo))
+    return (fibonacci(n-1) + fibonacci(n-2))
---
+++ stairmaster.py
@@ -0,0 +1,9 @@
+def climb_stairs(n):
+    if n == 0 or n == 1:
+        return 1
+    memo = [0] * (n + 1)
+    memo[0] = 1
+    memo[1] = 1
+    for i in range(2, n + 1):
+        memo[i] = memo[i-1] + memo[i-2]
+    return memo[n]
